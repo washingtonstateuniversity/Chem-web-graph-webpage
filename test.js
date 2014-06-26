42d7651a74f29d03cc7ebb7ca972f2b7efab733f
tiecheng.zhou <tiecheng.zhou@email.wsu.edu>
2014-06-26 11:54:07 -0700

    main page style file

---
+++ test.js
@@ -0,0 +1,16 @@
+      jQuery(document).ready(function() {
+
+          $('#myshowhide').on("click",function(){
+            $('#graphbox').toggle("slow");
+          }) //end of my show/hide
+
+          // first disable the graph editor and then send data to server
+          $('#mysend').on("click",function(){
+            $('#graphbox').block({message: '<h3>submitting</h3>'});
+
+          })//end of mysend 
+
+          $('#myreflesh').on("click",function(){
+            $('#graphbox').unblock();
+          })//end of myreflesh
+      });
